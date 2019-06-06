be26ee78808e0f029810c8b28036a8d19d6afe0f
Suraj Dadral <dadralj18@gmail.com>
2019-06-06 10:39:07 +0530

    Port code to python3
    Done changes to make code python3 compatible

--- building.py
+++ building.py
@@ -1,28 +1,26 @@
 from config import *
-import string
-import FreeCAD, Part#, PartGui
+import FreeCAD, Part
 from FreeCAD import Base
 ac_doc = FreeCAD.ActiveDocument
 
 # Process span input (from config file).
 def span_process(span_string):
-    print 234
     # Remove leading and trailing whitespaces, and put in "span_st" variable.
-    span_st=string.strip(span_string)
+    span_st=span_string.strip()
     # Split string contained in "span_st", where the "+" is encountered and put in span_sp array.
-    span_sp=string.split(span_st, '+')
+    span_sp=span_st.split('+')
     index=0
     # Empty Array.
     list=[]
     while index < len(span_sp):
 
         # Find "@" recursively in span_sp array. If not found, append the index value to "list" array.
-        if string.find(span_sp[index], '@')==-1:
+        if '@' not in span_sp[index]:
             list.append(float(span_sp[index]))
         else:
 
         # If found, split the index value, where "@" is encountered, and put in in_sp array.
-            in_sp=string.split(span_sp[index], '@')
+            in_sp=span_sp[index].split('@')
 
             # Append the value written after "@" to "list" array for no. of times equal to the value written before "@".
             count=0
@@ -56,8 +54,6 @@ dis_span_len = span_process(rep_span_len)
 
 # "dis_span_wid" array contains list of "spans" length wise.
 dis_span_wid = span_process(rep_span_wid)
-
-clear_height = span_process(cclear_height)
 
 # Number of spans building's length wise.
 no_spans_len = len(dis_span_len)
@@ -116,14 +112,10 @@ j = 0
 k = 0
 nodes = []
 z = 0
-print 0
 #Drawing ground plane to depict ground level
 plane([-3, -3, plinth_lev - 2 * plinth_lev], x_sum(no_spans_len) + 6, y_sum(no_span_wid) + 6)
-print 2
 #Drawing plinth plane to depict plinth level
 plane([0, 0, 0], x_sum(no_spans_len), y_sum(no_span_wid))
-
-print 1
 
 while z <= stories:
 	x = 0
